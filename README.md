97e6b0bd1c44d42dae5a1f1fc10e931ff3d05547
thekure <laku@itu.dk>
2022-12-29 13:10:39 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # examDisys
+
+Mandatory Hand-In 5
+== A Distributed Auction System ==
+
+------- INSTRUCTIONS ------------
+
+To run the program, open 3 separate terminals at the project directory.
+
+If you have MAKE installed (otherwise, see below)
+
+Run the following commands, one in each terminal:
+
+    make server0
+
+    make server1
+
+    make server2
+
+If you don't have MAKE:
+
+Run the following commands, one in each terminal:
+
+    go run server/server.go 0
+
+    go run server/server.go 1
+
+    go run server/server.go 2
+
+It is now possible to connect to the three servers by running the following in a seperate terminal:
+
+    go run client/client.go     
---
+++ client/client.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	examService "github.com/thekure/examDisys/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	name              string
+	currentHighestBid int32
+	servers           map[int32]examService.ServiceClient
+}
+
+func main() {
+
+	fmt.Println("Please enter a name")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanBytes)
+
+	// Set client name:
+	tempName := ""
+	for scanner.Scan() {
+		if scanner.Text() == "\n" {
+			break
+		} else {
+			tempName += scanner.Text()
+		}
+	}
+
+	// flag.Parse()
+	setLog()
+
+	client := &Client{
+		name:              tempName,
+		currentHighestBid: 0,
+		servers:           make(map[int32]examService.ServiceClient),
+	}
+
+	go handleClient(client)
+
+	for {
+
+	}
+
+}
+
+func (client *Client) makeBid(amount int32) {
+	bid := &examService.RequestBid{
+		Name:    client.name,
+		Message: client.name + " has made the following bid: " + strconv.Itoa(int(amount)),
+		Amount:  amount,
+	}
+
+	resultList := make([]string, 0, 3)
+
+	for port, server := range client.servers {
+		ack, err := server.Bid(context.Background(), bid)
+		if err != nil {
+			delete(client.servers, port)
+			log.Printf(client.name + "lost connection to a server, operating number of servers are now " + strconv.Itoa(int(len(resultList))))
+			// fmt.Printf("something went wrong in bid method: %v", err)
+		} else {
+			resultList = append(resultList, ack.GetMessage())
+		}
+
+	}
+	fmt.Printf(resultList[0] + "\n")
+}
+
+func (client *Client) requestHighestBid() {
+	reqBid := &examService.HighestBidRequest{
+		Message: client.name + " requested the current bid value.",
+	}
+	// outcome, err := client.connection.Result(context.Background(), reqBid)
+	// if err != nil {
+	// 	log.Printf(err.Error())
+	// }
+
+	resultList := make([]string, 0, 3)
+
+	for port, server := range client.servers {
+		outcome, err := server.Result(context.Background(), reqBid)
+		if err != nil {
+
+			delete(client.servers, port)
+			log.Printf(client.name + "lost connection to a server, operating number of servers are now " + strconv.Itoa(int(len(resultList))))
+			// fmt.Printf("something went wrong in requestHB method: %v", err)
+		} else {
+			resultList = append(resultList, outcome.GetStatus())
+		}
+
+	}
+	fmt.Printf(resultList[0] + "\n")
+}
+
+// handles clientinput
+func (client *Client) sendMessage() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		//Checks if the client input is of type string og integer (Tries to convert/parse, if an error occurs = string is not parseable)
+		amount, err := strconv.ParseInt(input, 10, 32)
+		if err != nil {
+			//Tell client that the current bid is at ... and to make a bid, type an integer
+			client.requestHighestBid()
+		} else {
+			client.makeBid(int32(amount))
+		}
+	}
+}
+
+func handleClient(client *Client) {
+	client.getServerConnection()
+
+	go client.sendMessage()
+
+	for {
+
+	}
+
+}
+
+// hardcoded method that connects to three different servers to ensure active replications
+func (client *Client) getServerConnection() {
+
+	for i := 0; i < 3; i++ {
+
+		port := int32(5001) + int32(i)
+		var conn *grpc.ClientConn
+
+		fmt.Printf("Trying to dial: %v\n", port)
+		insecure := insecure.NewCredentials()
+		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), grpc.WithTransportCredentials(insecure), grpc.WithBlock())
+		if err != nil {
+			log.Fatalf("Could not connect: %s", err)
+		}
+
+		fmt.Printf("--- "+client.name+" succesfully dialed to %v\n", port)
+		log.Printf("--- "+client.name+" succesfully dialed to %v\n", port)
+
+		// defer conn.Close()
+		c := examService.NewServiceClient(conn)
+		client.servers[port] = c
+	}
+
+}
+
+// Sets log output to file in project dir
+func setLog() *os.File {
+	// This connects to the log file/changes the output of the log informaiton to the log.txt file.
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	return f
+}
